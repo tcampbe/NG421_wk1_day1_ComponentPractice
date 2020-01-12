76d960ebf7546ad38bf3b4b59a49400d62152b01
tcampbe <tcampb@icloud.com>
2020-01-12 12:36:59 -0600

    component tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { DateTimeComponent } from './date-time/date-time.component';
-import { MessagepreviewComponent } from './messagepreview/messagepreview.component';
-import { TskItemComponent } from './tsk-item/tsk-item.component';
-import { MessagePreviewComponent } from './message-preview/message-preview.component';
-import { TaskItemComponent } from './task-item/task-item.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { CommentsComponent } from './comments/comments.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { OrdersComponent } from './orders/orders.component';
-import { TicketsComponent } from './tickets/tickets.component';
-import { AreaChartComponent } from './area-chart/area-chart.component';
-import { DonutChartComponent } from './donut-chart/donut-chart.component';
-import { TasksPanelComponent } from './tasks-panel/tasks-panel.component';
-import { TransactionsPanelComponent } from './transactions-panel/transactions-panel.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from "./app.component";
+import { DateTimeComponent } from "./date-time/date-time.component";
+import { MessagePreviewComponent } from "./message-preview/message-preview.component";
+import { TaskItemComponent } from "./task-item/task-item.component";
+import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SideNavComponent } from "./side-nav/side-nav.component";
+import { CommentsComponent } from "./comments/comments.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { OrdersComponent } from "./orders/orders.component";
+import { TicketsComponent } from "./tickets/tickets.component";
+import { AreaChartComponent } from "./area-chart/area-chart.component";
+import { DonutChartComponent } from "./donut-chart/donut-chart.component";
+import { TasksPanelComponent } from "./tasks-panel/tasks-panel.component";
+import { TransactionsPanelComponent } from "./transactions-panel/transactions-panel.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
-    MessagepreviewComponent,
-    TskItemComponent,
     MessagePreviewComponent,
     TaskItemComponent,
     TopNavComponent,
@@ -39,10 +35,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     TransactionsPanelComponent,
     DashboardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
