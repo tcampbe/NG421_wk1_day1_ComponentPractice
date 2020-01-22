f32b87f40e5abef3e5246a38059c8244381a0625
tcampbe <tcampb@icloud.com>
2020-01-22 11:46:14 -0600

    date time

--- src/app/date-time/date-time.component.ts
+++ src/app/date-time/date-time.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-date-time',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-time.component.css']
 })
 export class DateTimeComponent implements OnInit {
-
+@Input() private dateTime: string;
   constructor() { }
 
   ngOnInit() {
